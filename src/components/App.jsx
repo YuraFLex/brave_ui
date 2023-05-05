96d93487e5ab90fd6e6dc8eefc02cdc5f1b61ace
YuraFLex <shvalov.yurec@gmail.com>
2023-05-05 19:12:52 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,21 @@
-// import { Routes, Route } from 'react-router';
-// import { Layout } from './Layout/Layout';
-// import { DashBoardPage } from 'pages/DashBoardPage/DashBoardPage';
-// import { ReportsPage } from 'pages/ReportsPage/ReportsPage';
-import RegistrationForm from './RegisterForm/RegisterForm';
+import { Routes, Route } from 'react-router';
+import { Layout } from './Layout/Layout';
+import { DashBoardPage } from 'pages/DashBoardPage/DashBoardPage';
+import { ReportsPage } from 'pages/ReportsPage/ReportsPage';
+// import RegistrationForm from './RegisterForm/RegisterForm';
 // import LoginForm from './LoginForm/LoginForm';
 
 export const App = () => {
   return (
     <>
-      <RegistrationForm />
+      {/* <RegistrationForm /> */}
       {/* <LoginForm /> */}
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<DashBoardPage />} />
           <Route path="reports" element={<ReportsPage />} />
         </Route>
-      </Routes> */}
+      </Routes>
     </>
   );
 };

40355f4f0b4e5ea8ee9c9e4a2a6feea889b968fd
YuraFLex <shvalov.yurec@gmail.com>
2023-09-28 17:09:03 +0300

    Update urls.js

--- src/constans/urls.js
+++ src/constans/urls.js
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = 'http://204.62.13.40:3020/api'
-export const API_BASE_URL = 'http://localhost:3020/api'
+export const API_BASE_URL = 'http://204.62.13.40:3020/api'
+// export const API_BASE_URL = 'http://localhost:3020/api'
